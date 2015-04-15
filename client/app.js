f0d6ff6cd4fadeed7a8e5b6129ad2f4905bf6a8c
Ben <bhandy@udel.edu>
2015-04-15 18:44:41 +0000

    First commit

---
+++ client/app.js
@@ -0,0 +1,232 @@
+//we'll use routes for this one, our app is "upvoter"
+var app = angular
+  .module("reddit", ['ui.router']);
+
+app .config([
+  '$stateProvider',
+  '$urlRouterProvider', 
+  function($stateProvider, $urlRouterProvider){
+    $stateProvider
+      .state ('home', {
+        url: '/home',
+        templateUrl: "/home.html",
+        controller : "homeController",
+        resolve : {
+          postPromise: ["posts", function(posts){
+            return posts.fetchAll();}]
+        }
+      })
+
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: "/posts.html",
+        controller : "postController",
+        resolve : {
+          post: ["$stateParams", "posts", function($stateParams, posts) {
+            return posts.fetchOne($stateParams.id);
+          }]
+        }
+      })
+
+      .state('login', {
+        url: '/login',
+        templateUrl: '/login.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'auth', function($state, auth){
+          if(auth.isLoggedIn()){
+            $state.go('home');
+          }
+        }]
+      })
+
+      .state('register', {
+        url: '/register',
+        templateUrl: '/register.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'auth', function($state, auth){
+          if(auth.isLoggedIn()){
+            $state.go('home');
+          }
+        }]
+      });
+
+      $urlRouterProvider.otherwise('home');
+  }
+]);
+
+app.factory('posts', ["$http", 'auth', function($http, auth){
+
+  var object = {
+    posts : [],
+    fetchAll : function(){
+        return $http.get("/posts").success(function(data){
+            angular.copy(data, object.posts);
+        });
+    },
+    
+    fetchOne : function(id) {
+        return $http.get("/posts/"+id).then(function(res){  return res.data; });
+    },
+    
+    createPost : function(post) {
+        return $http.post("/posts", post, 
+            {headers: {Authorization: 'Bearer '+auth.getToken()}} ).success(function(data){
+            object.posts.push(data);
+        });
+    },
+    
+    addComment : function(id, comment) {
+        return $http.post("/posts/"+id+"/comments",comment, {headers: {Authorization: 'Bearer '+auth.getToken()}});
+    }, 
+ 
+    upvote : function(post){
+        return $http.put("/posts/" + post._id + "/upvote", null ,
+          {headers: {Authorization: 'Bearer '+auth.getToken()}})
+          .success(function(data){
+            post.upvotes += 1;
+          });
+    },
+
+    upvoteComment : function(post, comment) {
+      return $http.put("/posts/"+post._id + "/comments/"+comment._id + "/upvote", null ,
+        {headers: {Authorization: 'Bearer '+auth.getToken()}})
+        .success(function(data){
+          comment.upvotes += 1;
+        });
+    }
+  };
+
+  return object;
+
+  }]);
+
+app.factory('auth', ['$http', '$window', function($http, $window){
+      var auth = {
+      
+      saveToken : function (token){
+        $window.localStorage['reddit-token'] = token;
+      },
+
+      getToken : function(token) {
+        return $window.localStorage['reddit-token'];
+      },
+
+      isLoggedIn : function() {
+        var token = auth.getToken();
+
+        if(token) {
+          var payload = JSON.parse($window.atob(token.split('.')[1]));
+          return payload.exp > Date.now() / 1000;
+        } else {
+          return false;
+        }
+      },
+
+      currentUser : function() {
+        if(auth.isLoggedIn()) {
+          var token = auth.getToken();
+          var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+          return payload.username;
+        }
+      },
+
+      register : function(user) {
+        return $http.post('/register', user).success(function(data){
+          auth.saveToken(data.token);
+        });
+      },
+
+      logIn : function(user) {
+        return $http.post('/login', user).success(function(data){
+          auth.saveToken(data.token);
+        });
+      },
+
+      logOut : function() {
+        $window.localStorage.removeItem('reddit-token');
+      }
+  };
+
+  return auth;
+}])
+
+
+app.controller(
+  "postController", 
+  ["$scope", "posts", 'post', 'auth',
+    function($scope, posts, post, auth){
+      $scope.text = "";
+
+      $scope.post = post;
+      $scope.comments = post.comments;
+      $scope.isLoggedIn = auth.isLoggedIn;
+
+      $scope.addComment = function(){
+        if (!$scope.text || $scope.text === ''){ return; }
+
+        posts.addComment(post._id, {text: $scope.text, upvotes: 0})
+          .success(function(comment) {
+            $scope.comments.push(comment);
+        });
+        $scope.text = '';
+      }
+      $scope.increaseCommentUpvotes = function(comment){
+        posts.upvoteComment(post, comment);
+      }
+    }
+  ]
+);
+
+app.controller(
+  "homeController", 
+  ["$scope", "posts", 'auth',
+    function($scope, posts, auth){
+      $scope.text = "";
+      $scope.posts = posts.posts; 
+      $scope.isLoggedIn = auth.isLoggedIn;
+      $scope.addPost = function(){
+        if (!$scope.text || $scope.text === ''){ return; }
+        posts.createPost({text: $scope.text, upvotes: 0, comments: []});
+        $scope.text = '';
+      }
+
+      $scope.increaseUpvotes = function(post){
+        posts.upvote(post);
+      }
+    }
+  ]
+);
+
+app.controller('AuthCtrl', [
+'$scope',
+'$state',
+'auth',
+function($scope, $state, auth){
+  $scope.user = {};
+
+  $scope.register = function(){
+    auth.register($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+
+  $scope.logIn = function(){
+    auth.logIn($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+}])
+
+app.controller('NavCtrl', [
+'$scope',
+'auth',
+function($scope, auth){
+  $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.currentUser = auth.currentUser;
+  $scope.logOut = auth.logOut;
+}]);
---
+++ server.js
@@ -0,0 +1,239 @@
+var http = require('http');
+var path = require('path');
+var express = require('express');
+var router = express();
+var server = http.createServer(router);
+
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+//Authentication
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var ejwt = require('express-jwt');
+var auth = ejwt({secret: 'SECRET', userProperty: 'payload'});
+
+//How to authenticate a user
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+router.use( bodyParser.json() );
+
+router.use(passport.initialize());
+
+router.use(express.static(path.resolve(__dirname, 'client')));
+
+mongoose.connect("mongodb://localhost:19143/test");
+
+var PostSchema = new mongoose.Schema({ 
+  text: String,
+  upvotes: {type: Number, default: 0},
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+});
+
+PostSchema.methods.upvote = function(cb) {
+  this.upvotes += 1;
+  this.save(cb);
+};
+
+mongoose.model('Post', PostSchema);
+
+var CommentSchema = new mongoose.Schema({
+  text: String,
+  upvotes: {type: Number, default: 0},
+  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
+});
+
+CommentSchema.methods.upvote = function(cb) {
+  this.upvotes += 1;
+  this.save(cb);
+};
+
+mongoose.model('Comment', CommentSchema);
+
+var UserSchema = new mongoose.Schema({
+  username: {type: String, lowercase: true, unique: true},
+  hash: String,
+  salt: String
+});
+
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+
+  return this.hash === hash;
+};
+
+UserSchema.methods.generateJWT = function() {
+
+  // set expiration to 60 days
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    _id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000),
+  }, 'SECRET');
+};
+
+mongoose.model('User', UserSchema);
+
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
+
+/*
+GET /posts (fetch all posts, low detail)
+GET /posts/:postid (fetch all of the data for one post (all comments))
+POST /posts (create a new post using the posted data and return the new post data including the freshly created _id)
+POST /posts/:postid/comments (create a new comment using the posted data and return the new comment including _id)
+PUT /posts/:postid/upvote (upvotes the target post)
+PUT /posts/:postid/comments/:commentid/upvotes (upvotes the target comment)
+*/
+
+router.get("/posts", function(req, res, next){
+  Post.find(function(err, posts){
+    if (err) { return next(err); }
+    res.json(posts);
+  });
+});
+
+router.post('/posts', auth, function(req, res, next) {
+  var post = new Post(req.body);
+  post.author = req.payload.username;
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+
+});
+
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error('can\'t find comment')); }
+
+    req.comment = comment;
+    return next();
+  });
+});
+
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.post('/posts/:post/comments', auth, function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+  comment.author = req.payload.username;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
+  req.comment.upvote(function(err, comment){
+    if (err) { return next(err); }
+
+    res.json(comment);
+  });
+});
+
+//Authentication API
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password)
+
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
+});
+
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
+});
+
+server.listen(24266 || 3000, "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Upvote server listening at", addr.address + ":" + addr.port);
+});
